4d755f0ea0a00d23ed0804d74b5814d507834193
Jesse Lee <jelee85@gmail.com>
2025-11-24 15:56:23 -0500

    Fix Electron to use CommonJS and remove import.meta

--- desktop/electron/main.ts
+++ desktop/electron/main.ts
@@ -1,9 +1,5 @@
 import { app, BrowserWindow, ipcMain } from 'electron'
 import path from 'path'
-import { fileURLToPath } from 'url'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 
 let mainWindow: BrowserWindow | null = null
 
